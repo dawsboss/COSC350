3f2ead3eee6a9032da5df05ab68cccc965cc469f
Grant <gdawson2018@gmail.com>
2021-03-08 19:42:35 -0500

    task4 mainly done

--- Lab04/task04/task4.c
+++ Lab04/task04/task4.c
@@ -24,14 +24,30 @@ int main(int argc, char** argv){
 
     strcat(dir1, "/Dir1");
     strcat(dir2, "/Dir2");
-    strcat(dir12, "/Dir2/Dir12");
+    strcat(dir12, "/Dir2/Dir21");
     strcpy(helloCpy, dir12);
     strcat(helloCpy, "/hello");
 
-    printf("home: %s\ndir1: %s\ndir2: %s\ndir12: %s\n",home, dir1, dir2, dir12);
-    if( (mkdir(dir1, 0775) > 0)||(mkdir(dir2, 0775))||(mkdir(dir12, 0775))){
-        printf("Error creating dir\n");
-        return 1;
+    //printf("home: %s\ndir1: %s\ndir2: %s\ndir12: %s\n",home, dir1, dir2, dir12);
+    //Do we need to make?
+    struct stat buff;
+    if(stat(dir1, &buff)<0){
+        if(mkdir(dir1, 0775) < 0){
+            printf("Error creating Dir1\n");
+            return 1;
+        }
+    }
+    if(stat(dir2, &buff) < 0){
+        if(mkdir(dir2, 0775) < 0){
+            printf("Error creating Dir2\n");
+            return 1;
+        }
+    }
+    if(stat(dir12, &buff) < 0){
+        if(mkdir(dir12, 0775) < 0){
+            printf("Error creating Dir21\n");
+            return 1;
+        }
     }
 
     int cpy = open(helloCpy, O_WRONLY|O_CREAT, 0777);
@@ -56,8 +72,31 @@ int main(int argc, char** argv){
         printf("Error reading\n");
         return 1;
     }
-
-
     close(cpy);
+
+    //linking
+    char* toHello = malloc(len+5+8);
+    strcpy(toHello, dir1);
+    strcat(toHello, "/toHello");
+
+    char* toDir21 = malloc(len+5+8);
+    strcpy(toDir21, dir1);
+    strcat(toDir21, "/toDir21");
+
+
+    if(stat(toHello, &buff)<0){
+        if(symlink(helloCpy, toHello) < 0){
+            printf("Error symlink toHello!\n");
+            return 1;
+        }
+    }
+
+    if(stat(toDir21, &buff)<0){
+        if(symlink(dir12, toDir21) < 0){
+            printf("\n");
+            return 1;
+        }
+    }
+
     return 0;
 }
