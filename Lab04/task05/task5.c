3771a60b87a1b426b3afef6a5088c32bbdfd144b
Grant <gdawson2018@gmail.com>
2021-03-09 14:10:07 -0500

    More to lab4 task5 very close

--- Lab04/task05/task5.c
+++ Lab04/task05/task5.c
@@ -16,18 +16,11 @@ int main(int argc, char** argv){
 
     int len = strlen(argv[2]);
     char* newPath; 
+    len = strlen(argv[1]);
+    char* oldPath;
     
-        
-
-    struct stat buf;
-    if(stat(argv[2], &buf) < 0){
-        printf("Invalid Desination or Destination does not exist\n");
-        return 1;
-    }
-
-
     if(argv[2][0]=='~'){
-        len+=(strlen(getenv("HOME")) - 1);//Take one for ~ being replced
+        len+=(strlen(getenv("HOME")) - 2);//Take one for ~ being replced
         newPath = malloc(len);
         strcpy(newPath, getenv("HOME"));
         strcat(newPath, argv[2]);
@@ -35,14 +28,45 @@ int main(int argc, char** argv){
         newPath = malloc(len);
         strcat(newPath, argv[2]);
     }
-
-
-    if(S_ISREG(buf.st_mode)){
-        
+   
+    if(argv[1][0]=='~'){
+        len+=(strlen(getenv("HOME")) - 2);//Take one for ~ being replced
+        oldPath = malloc(len);
+        strcpy(oldPath, getenv("HOME"));
+        strcat(oldPath, argv[1]);
     }else{
-    
+        oldPath = malloc(len);
+        strcat(oldPath, argv[1]);
     }
 
+    printf("oldPath: %s\nnewPath: %s\n", oldPath, newPath);
+    //if(access(oldPath, F_OK) < 0){
+    //    printf("Can't access given file to move!\n");
+    //    return 1;
+    //}
+
+
+
+    struct stat buf;
+    if(stat(argv[2], &buf) < 0){//Either the Directory DOesn;t exits or we need to rename so try rename and if that doesn;t work then directory doesn;t exist
+        if(rename(oldPath, newPath) < 0){
+            printf("Invalid Desination or Destination does not exist\n");
+            return 1;
+        }
+    }//otherwise There is a directory already so we can move safetly
     
+    if(link(oldPath, newPath) < 0){
+        printf("Error linking to move\n");
+        return 1;
+    }
+    if(unlink(oldPath) < 0){
+        printf("Error unlinking\n");
+        return 1;
+    }
+    
+
+
+
+
     return 0;
 }
